f56d2708fc75baaa60970f5d03a359e1a7902f47
QuentinSup <qsupernant@gmail.com>
2013-06-20 13:01:41 +0200

    Make date dynamic
    Next event is automatically updated

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
 $(document).ready(function() {
 
+	var getEventDate = function(now) {
+		var day = now.getDay();
+		var date = now.getDate();
+		var newDate = new Date(now.getTime());
+		newDate.setDate(1);
+		var firstDayOfMonth = newDate.getDay();
+		var eventDate = new Date(now.getTime());
+		if(firstDayOfMonth > 4) {
+			eventDate.setDate((6 - firstDayOfMonth) + 4);
+		} else {
+			eventDate.setDate(5 - firstDayOfMonth);
+		}
+		return eventDate;
+	};
+
+	var getNextEventDate = function() {
+		var now = new Date();
+		var eventDate = getEventDate(now);
+		if(now.getDate() > eventDate.getDate()) {
+			now.setMonth(now.getMonth() + 1);
+			now.setDate(1);
+			eventDate = getEventDate(now);
+		}
+
+		eventDate.setHours(19);
+		eventDate.setMinutes(00);
+
+		return eventDate;
+	};
+
+	var getNextEventDateString = function() {
+		var date = getNextEventDate();
+		var months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+		return 'Jeudi ' + date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear() + ' 19h00'; 
+	};
+
 	var organizersArray = [
 		{ name: 'Quentin Supernant', twitter:'@QuentinSup' },
 		{ name: 'Mathieu Genty', twitter:'@misterniark' },
@@ -7,7 +43,7 @@ $(document).ready(function() {
 		{ name: 'Julien Lebeau', twitter:'@_Jlebeau_' }
 	];
 
-	var _nextDate = ko.observable('6 juin 2013');
+	var _nextDate = ko.observable(getNextEventDateString());
 
 	var _organizers = ko.observableArray(organizersArray);
 	var view = {
