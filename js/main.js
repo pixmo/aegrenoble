efb2e1a8aa024f8237971f0c2c90db4cd43d2954
QuentinSup <qsupernant@gmail.com>
2014-02-17 18:19:01 +0100

    Modify GO list

--- js/main.js
+++ js/main.js
@@ -41,9 +41,11 @@ $(document).ready(function() {
 
 	var organizersArray = [
 		{ name: 'Quentin Supernant', twitter:'@QuentinSup' },
-		{ name: 'Mathieu Genty', twitter:'@misterniark' },
+		{ name: 'Mathieu Genty', twitter:'@misterniark' }
+		/*
 		{ name: 'Allan Francani', twitter:'@AFrancani' },
 		{ name: 'Julien Lebeau', twitter:'@_Jlebeau_' }
+		*/
 	];
 
 	var _nextDate = ko.observable(getNextEventDateString());
